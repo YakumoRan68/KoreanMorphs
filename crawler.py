753da6370a3bab28e318aba797d02ada69554bc1
YakumoRan <37825675+YakumoRan68@users.noreply.github.com>
2019-11-05 11:38:19 +0900

    1105
    한게 없다

--- crawler.py
+++ crawler.py
@@ -1,6 +1,9 @@
+import requests
 from bs4 import BeautifulSoup
 
-with open("example.html") as fp:
-    soup = BeautifulSoup(fp, 'html.parser')
-    all_divs = soup.find_all("div")
-    print(all_divs)
+req = requests.get("http://news.khan.co.kr/kh_news/khan_art_view.html?artid=201911050854001&code=940202")
+html = req.text
+soup = BeautifulSoup(fp, 'html.parser')
+
+div = soup.find("content_text")
+print(all_divs)
