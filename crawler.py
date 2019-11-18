7f5fef16a84fd8f58ffaf6fbbd93eb82bc9d4dda
YakumoRan68 <yys14239103@gmail.com>
2019-11-18 22:35:48 +0900

    1118

--- crawler.py
+++ crawler.py
@@ -1,5 +1,7 @@
 import requests
 import re
+import datetime
+import io
 from time import sleep
 from korea_news_crawler.articleparser import ArticleParser
 from korea_news_crawler.articlecrawler import ArticleCrawler
@@ -8,6 +10,9 @@ from bs4 import BeautifulSoup
 category = "001" #속보
 NEWS_LIST_URL = "http://news.naver.com/main/list.nhn?mode=LSD&mid=sec&sid1=" + category + "&date="
 NEWS_LIST_URL_BY_DATE = ArticleCrawler().make_news_page_url(NEWS_LIST_URL, 2019, 2019, 11, 11)
+
+NOW = datetime.datetime.now()
+FILE_NAME = (str)(NOW.year) + "-" + (str)(NOW.month) + "-" + (str)(NOW.day) + "-" + (str)(NOW.hour) + "-" + (str)(NOW.minute)
 
 def get_url_data(url, max_tries=10): #referenced from : https://github.com/lumyjuwon/KoreaNewsCrawler/blob/master/korea_news_crawler/articlecrawler.py
     remaining_tries = int(max_tries)
@@ -19,51 +24,48 @@ def get_url_data(url, max_tries=10): #referenced from : https://github.com/lumyj
         remaining_tries = remaining_tries - 1
     raise ResponseTimeout()
 
-for URL in NEWS_LIST_URL_BY_DATE : #referenced from : https://github.com/lumyjuwon/KoreaNewsCrawler/blob/master/korea_news_crawler/articlecrawler.py
-    regex = re.compile("date=(\d+)")
-    news_date = regex.findall(URL)[0]
-    request = get_url_data(URL)
-    document = BeautifulSoup(request.content, 'html.parser')
+with io.open(FILE_NAME, 'w', encoding = 'utf-8') as output :
+    for URL in NEWS_LIST_URL_BY_DATE : #referenced from : https://github.com/lumyjuwon/KoreaNewsCrawler/blob/master/korea_news_crawler/articlecrawler.py
+        regex = re.compile("date=(\d+)")
+        news_date = regex.findall(URL)[0]
+        request = get_url_data(URL)
+        document = BeautifulSoup(request.content, 'html.parser')
 
-    # html - newsflash_body - type06_headline, type06
-    # 각 페이지에 있는 기사들 가져오기
-    post_temp = document.select('.newsflash_body .type06_headline li dl')
-    post_temp.extend(document.select('.newsflash_body .type06 li dl'))
+        # html - newsflash_body - type06_headline, type06
+        # 각 페이지에 있는 기사들 가져오기
+        post_temp = document.select('.newsflash_body .type06_headline li dl')
+        post_temp.extend(document.select('.newsflash_body .type06 li dl'))
 
-    post = []
-    MAXCOUNT = 0
-    for line in post_temp:
-        if MAXCOUNT >= 10 :
-            break
-        post.append(line.a.get('href')) # 해당되는 page에서 모든 기사들의 URL을 post 리스트에 넣음
-        MAXCOUNT += 1
-    del post_temp
-    
-    MAXCOUNT = 0
-    for content_url in post:  # 기사 URL
-        if MAXCOUNT >= 1 :
-            break
-        # 크롤링 대기 시간
-        sleep(0.01)
-        
-        # 기사 HTML 가져옴
-        request_content = get_url_data(content_url)
-        try:
-            document_content = BeautifulSoup(request_content.content, 'html.parser')
-        except:
-            continue
+        post = []
+        MAXCOUNT = 0
+        for line in post_temp:
+            post.append(line.a.get('href')) # 해당되는 page에서 모든 기사들의 URL을 post 리스트에 넣음
+        del post_temp
 
-        try:
-            text_sentence = ""
-
-            tag_content = document_content.find_all('div', {'id': 'articleBodyContents'})
-            text_sentence = text_sentence + ArticleParser.clear_content(str(tag_content[0].find_all(text=True)))
-            if not text_sentence:  # 공백일 경우 기사 제외 처리
+        MAXCOUNT = 0
+        for content_url in post:  # 기사 URL
+            if MAXCOUNT >= 1 :
+                break
+            # 크롤링 대기 시간
+            sleep(0.01)
+            
+            # 기사 HTML 가져옴
+            request_content = get_url_data(content_url)
+            try:
+                document_content = BeautifulSoup(request_content.content, 'html.parser')
+            except:
                 continue
 
-            print(text_sentence)
+            try:
+                text_sentence = ""
 
-        except Exception as ex:
-            del request_content, document_content
-            pass
+                tag_content = document_content.find_all('div', {'id': 'articleBodyContents'})
+                text_sentence = text_sentence + ArticleParser.clear_content(str(tag_content[0].find_all(text=True)))
+                if not text_sentence:  # 공백일 경우 기사 제외 처리
+                    continue
 
+                output.write(text_sentence)
+
+            except Exception as ex:
+                del request_content, document_content
+                pass
